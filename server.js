62edb421fd7c3464ba008cd405f6dc460b778d5c
Suchit <suchit844118@gmail.com>
2026-03-06 08:21:32 +0530

    ready for deployement code

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
 const connectDb = require('./config/connectDb');
+const path=require('path');
 //config dotenv
 dotenv.config();
 //connectDB
@@ -24,6 +25,12 @@ app.use(cors());
 app.use('/api/v1/users', require('./routes/userRoute'));
 //transaction routes
 app.use('/api/v1/transactions', require('./routes/transactionRoutes'));
+
+//static files
+app.use(express.static(path.join(__dirname, "./client/dist") ));
+app.use((req,res)=>{
+  res.sendFile(path.join(__dirname,"client/dist/index.html"));
+});
 //PORT
 const PORT = process.env.PORT || 8080;
 console.log("USING MONGO URL =>", process.env.MONGO_URL);
